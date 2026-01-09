c9721f661df6e03d397967dcdce34179de458b2a
Joshua Bielas <joshua.bielas@wallawalla.edu>
2026-01-09 12:32:04 -0800

    adding flexbox styling and media queries for the home page

--- website/src/pages/home-page.jsx
+++ website/src/pages/home-page.jsx
@@ -4,14 +4,22 @@ import logo from '../images/TheWashZoneLogo.jpg';
 function HomePage() {
       return (
     <>
-    <HamburgerMenu />
-      <div className ="title">
-        The Wash Zone
+    <div className="hamburger-hidden">
+      <HamburgerMenu />
+    </div>
+    <div className ="home-flex">
+      <div>
+        <div className ="title">
+          The Wash Zone
+        </div>
+        <div className ="subtitle">
+          1907 E Isaacs Ave, Walla Walla, WA 99362
+        </div>
       </div>
-      <div className ="subtitle">
-        1907 E Isaacs Ave, Walla Walla, WA 99362
+      <div>
+         <img src={logo} alt="The Wash Zone Logo" />
       </div>
-      <img src={logo} alt="The Wash Zone Logo" />
+    </div>
     </>
   )
 }

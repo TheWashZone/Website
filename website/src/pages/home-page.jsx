b758194e8d149c8209c875e80b89fcef2a0d3c01
Joshua Bielas <joshua.bielas@wallawalla.edu>
2026-01-09 12:43:49 -0800

    adding a navbar

--- website/src/pages/home-page.jsx
+++ website/src/pages/home-page.jsx
@@ -1,9 +1,18 @@
 import HamburgerMenu from '../components/hamburger-menu';
 import logo from '../images/TheWashZoneLogo.jpg';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function HomePage() {
       return (
     <>
+    <div className="hide-navbar">
+      <Navbar bg="primary" className="fixed-top">
+        <Container>
+          <Navbar.Brand href="/" className="text-white">The Wash Zone</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </Container>
+      </Navbar>
+    </div>
     <div className="hamburger-hidden">
       <HamburgerMenu />
     </div>

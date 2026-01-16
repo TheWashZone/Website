1a5e08c692f8dd71af5a69871818067b1c39479f
Joshua Bielas <joshua.bielas@wallawalla.edu>
2026-01-15 19:46:14 -0800

    boxed home UI

--- website/src/pages/home-page.jsx
+++ website/src/pages/home-page.jsx
@@ -1,11 +1,20 @@
+import location from '../images/temp-washzone-picture.jpg';
+import banner from '../images/ChatGPT-Soapy-pavement.png';
 import logo from '../images/TheWashZoneLogo.jpg';
+
+
+import '../css/home-page.css';
+// THIS PICTURE SHOULD BE REPLACED WHEN WE HAVE ANOTHER ONE
 
 
 function HomePage() {
       return (
     <>
+    <div className="container-fluid p-0 w-100 header-container">
+      <img src={banner} className="header-img" alt="Responsive image" />
+    </div>
     <div className ="home-flex">
-      <div>
+      <div className = "boxed">
         <div className ="title">
           The Wash Zone
         </div>
@@ -14,7 +23,15 @@ function HomePage() {
         </div>
       </div>
       <div>
-         <img src={logo} alt="The Wash Zone Logo" />
+         <img src={location} alt="The Wash Zone Image" />
+      </div>
+      <div className="boxed">
+        <span className="subtitle">Memberships</span>
+        <p>memberships</p>
+      </div>
+      <div className="boxed">
+        <span className="subtitle">Hours of Operation</span>
+          <p>hours</p>
       </div>
     </div>
     </>

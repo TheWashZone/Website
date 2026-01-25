fed3dabb1fdb18e86ff7aae3810c210932e3a8c0
Joshua Bielas <joshua.bielas@wallawalla.edu>
2026-01-25 13:10:20 -0800

    checking in work before beginning writing tests

--- website/vitest.config.js
+++ website/vitest.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { describe, it, expect} from 'vitest';
 
 export default defineConfig({
   plugins: [react()],

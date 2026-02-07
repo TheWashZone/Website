17d44cf5017002861700f761725211e36daa9789
Joshua Bielas <joshua.bielas@wallawalla.edu>
2026-02-06 16:55:57 -0800

    trying new formatting for home page. Not quite complete

--- website/src/pages/home-page.jsx
+++ website/src/pages/home-page.jsx
@@ -7,81 +7,81 @@ import '../css/home-page.css';
 function HomePage() {
   return (
     <>
-      <div className="container-fluid p-0 w-100 header-container">
+      <div className="container-fluid">
         <img src={banner} className="header-img" alt="banner image" />
       </div>
-      <div className="home-format">
-        <div className="colored">
-          <div className="title">
+      <div className="">
+        <div className="blue-background">
+          <div className="title-text">
             The Wash Zone
           </div>
-          <div className="subtitle">
+          <div className="address-text">
             1907 E Isaacs Ave, Walla Walla, WA 99362
           </div>
         </div>
-        <div className="colored2">
-          <img src={location} alt="The Wash Zone Image" />
+        <div>
+          <img src={location} alt="The Wash Zone Image" className="carwash-img"/>
         </div>
-        <div className="colored">
-          <div className="membership-section-title">Memberships</div>
-          <div className="membership-container">
-            <div className="boxed membership-item" data-testid="box1">
-              <div className="membership-title">Ultimate - $16.50</div>
+        <div className="blue-background-memberships">
+          <div className="membership-title">Memberships</div>
+          <div className="membership-flex">
+            <div className="membership-item" data-testid="box1">
+              <div className="membership-item-title">Ultimate - $16.50</div>
               <ul>
-                <li className="left-align" data-testid="ultimate1">1</li>
-                <li className="left-align" data-testid="ultimate2">2</li>
-                <li className="left-align" data-testid="ultimate3">3</li>
-                <li className="left-align" data-testid="ultimate4">4</li>
-                <li className="left-align" data-testid="ultimate5">5</li>
+                <li className="" data-testid="ultimate1">1</li>
+                <li className="" data-testid="ultimate2">2</li>
+                <li className="" data-testid="ultimate3">3</li>
+                <li className="" data-testid="ultimate4">4</li>
+                <li className="" data-testid="ultimate5">5</li>
               </ul>
             </div>
-            <div className="boxed membership-item" data-testid="box2">
-              <div className="membership-title">Deluxe - $13.50</div>
+            <div className="membership-item" data-testid="box2">
+              <div className="membership-item-title">Deluxe - $13.50</div>
               <ul>
-                <li className="left-align" data-testid="deluxe1">1</li>
-                <li className="left-align" data-testid="deluxe2">2</li>
-                <li className="left-align" data-testid="deluxe3">3</li>
-                <li className="left-align" data-testid="deluxe4">4</li>
+                <li className="" data-testid="deluxe1">1</li>
+                <li className="" data-testid="deluxe2">2</li>
+                <li className="" data-testid="deluxe3">3</li>
+                <li className="" data-testid="deluxe4">4</li>
               </ul>
             </div>
-            <div className="boxed membership-item" data-testid="box3">
-              <div className="membership-title">Basic - $10.00</div>
+            <div className="membership-item" data-testid="box3">
+              <div className="membership-item-title">Basic - $10.00</div>
               <ul>
-                <li className="left-align" data-testid="basic1">1</li>
-                <li className="left-align" data-testid="basic2">2</li>
+                <li className="" data-testid="basic1">1</li>
+                <li className="" data-testid="basic2">2</li>
               </ul>
-              <p className="basic-disclaimer">No longer accepting new subscriptions for this wash type</p>
+              <p className="">No longer accepting new subscriptions for this wash type</p>
             </div>
-            <div className="boxed membership-item" data-testid="box4">
-              <div className="membership-title">Add Ons</div>
+            <div className="membership-item" data-testid="box4">
+              <div className="membership-item-title">Add Ons</div>
               <ul>
-                <li className="left-align" data-testid="addOn1">Wax: $0.00</li>
-                <li className="left-align" data-testid="addOn2">Wheel Brite: $0.00</li>
-                <li className="left-align" data-testid="addOn3">Floor Mats: $0.00</li>
-                <li className="left-align" data-testid="addOn4">Hand Wash: $0.00</li>
+                <li className="" data-testid="addOn1">Wax: $0.00</li>
+                <li className="" data-testid="addOn2">Wheel Brite: $0.00</li>
+                <li className="" data-testid="addOn3">Floor Mats: $0.00</li>
+                <li className="" data-testid="addOn4">Hand Wash: $0.00</li>
               </ul>
             </div>
-            <div className="boxed membership-item" data-testid="box5">
-              <div className="membership-title">Single Wash</div>
+            <div className="membership-item" data-testid="box5">
+              <div className="membership-item-title">Single Wash</div>
               <ul>
-                <li className="left-align" data-testid="singleWash1">TEMP: $0.00</li>
-                <li className="left-align" data-testid="singleWash2">TEMP: $0.00</li>
-                <li className="left-align" data-testid="singleWash3">TEMP: $0.00</li>
-                <li className="left-align" data-testid="singleWash4">TEMP: $0.00</li>
+                <li className="" data-testid="singleWash1">TEMP: $0.00</li>
+                <li className="" data-testid="singleWash2">TEMP: $0.00</li>
+                <li className="" data-testid="singleWash3">TEMP: $0.00</li>
+                <li className="" data-testid="singleWash4">TEMP: $0.00</li>
               </ul>
             </div>
           </div>
         </div>
-        <div className="colored2">
-          <span className="hours-parent">Hours of Operation</span>
-          <Stack gap={3} className="hours-stack" data-testid="hours-box">
-            <div className="hours">Sunday: 9:00 AM - 5:00 PM</div>
-            <div className="hours">Monday: 7:30 AM - 7:00 PM</div>
-            <div className="hours">Tuesday: 7:30 AM - 7:00 PM</div>
-            <div className="hours">Wednesday: 7:30 AM - 7:00 PM</div>
-            <div className="hours">Thursday: 7:30 AM - 7:00 PM</div>
-            <div className="hours">Friday: 7:30 AM - 7:00 PM</div>
-            <div className="hours">Saturday: 8:00 AM - 7:00 PM</div>
+        <div className="hours-section">
+          <span className="hours-title">Hours of Operation</span>
+          <Stack gap={3} className="hours-item" data-testid="hours-box">
+            <div className="">Sunday: 9:00 AM - 5:00 PM</div>
+            <div className="">Monday: 7:30 AM - 7:00 PM</div>
+            <div className="">Tuesday: 7:30 AM - 7:00 PM</div>
+            <div className="">Wednesday: 7:30 AM - 7:00 PM</div>
+            <div className="">Thursday: 7:30 AM - 7:00 PM</div>
+            <div className="">Friday: 7:30 AM - 7:00 PM</div>
+            <div className="">Saturday: 8:00 AM - 7:00 PM</div>
           </Stack>
         </div>
       </div>

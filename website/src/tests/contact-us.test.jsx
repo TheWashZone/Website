d3ba0c241cfa0b103f1f268ea70405fc037e8905
Joshua Bielas <joshua.bielas@wallawalla.edu>
2026-03-01 12:49:00 -0800

    more fixes

--- website/src/tests/contact-us.test.jsx
+++ website/src/tests/contact-us.test.jsx
@@ -1,5 +1,6 @@
 import { describe, it, expect} from 'vitest';
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ContactPage from '../pages/contact-us.jsx';
 
